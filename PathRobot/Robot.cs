4b5c72ed8d3a9fc301245c1f15f455c960c14161
TordTroen <assessedgrub@gmail.com>
2016-04-26 17:36:55 +0200

    Initial commit

---
+++ PathRobot/Robot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace PG4500_2016_Exam2
+{
+    public class Trotor14_MechaGodzilla : AdvancedRobot
+    {
+		public override void Run()
+		{
+			// https://www.youtube.com/watch?v=T7Fy5w2klbg
+		}
+	}
+}
