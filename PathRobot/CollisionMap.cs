af2ebbe266e7bcd2d10928781f19976b006bb507
TordTroen <assessedgrub@gmail.com>
2016-04-27 13:34:13 +0200

    Added nodes and nodemap

---
+++ PathRobot/CollisionMap.cs
@@ -0,0 +1,55 @@
+﻿using PG4500_2016_Exam2;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeExam
+{
+	public class CollisionMap
+	{
+		public const double NodeSize = 50;
+
+		public MapNode[] map;
+		private int[] simpleMap = new int[] 
+		{
+			0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+			1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+			1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+			0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+			0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+			0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+			0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+			0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		};
+		private readonly Trotor14_MechaGodzilla robot;
+
+		public CollisionMap(Trotor14_MechaGodzilla robot)
+		{
+			this.robot = robot;
+			InitializeMap();
+		}
+
+		private void InitializeMap()
+		{
+			for (int i = 0; i < simpleMap.Length; i ++)
+			{
+
+			}
+		}
+
+		public void PaintMap()
+		{
+			foreach (var node in map)
+			{
+				robot.Drawing.DrawBox(Color.White, )
+			}
+		}
+	}
+}
---
+++ PathRobot/MapNode.cs
@@ -0,0 +1,28 @@
+﻿using HomeExam.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeExam
+{
+	public class MapNode
+	{
+		public int X { get; private set; }
+		public int Y { get; private set; }
+		public bool IsObstacle { get; private set; }
+
+		public MapNode(int x, int y, bool isObstacle)
+		{
+			X = x;
+			Y = y;
+			IsObstacle = isObstacle;
+		}
+
+		public Vector2D GetPhysicalPosition()
+		{
+			return new Vector2D(X * CollisionMap.NodeSize, Y * CollisionMap.NodeSize);
+		}
+	}
+}
